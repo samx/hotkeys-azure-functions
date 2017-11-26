0c5e422bed20365d5e6c318ebaa4a3881952abb5
samx-mac <samsono@gmail.com>
2017-11-25 20:34:43 -0500

    update6

--- CheckYoutubeAPIForClosedCaptions/index.js
+++ CheckYoutubeAPIForClosedCaptions/index.js
@@ -1,7 +1,5 @@
 module.exports = function (context, req) {
-    context.log('Update test5:JavaScript HTTP trigger function processed a request.');
-
-
+    context.log('Update test7:JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
