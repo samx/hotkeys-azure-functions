c905837026ad927745468891f76c4691ba6c2f46
samx <samsono@gmail.com>
2017-11-25 20:58:13 -0500

    update8

--- .history/CheckYoutubeAPIForClosedCaptions/index_20171125205726.js
+++ .history/CheckYoutubeAPIForClosedCaptions/index_20171125205726.js
@@ -1,5 +1,5 @@
 module.exports = function (context, req) {
-    context.log('Update test8:JavaScript HTTP trigger function processed a request.');
+    context.log('Update test7:JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
